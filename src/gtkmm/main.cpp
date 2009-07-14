d8e6806fa0349d368490c7c04eb2d33b6245cbd2
Spencer Andrew Jackson <spencerandrewjackson@gmail.com>
2009-07-14 12:24:26 -0500

    Add a barebones gtkmm app
    This will be expanded for the main application
    CMake scrips were updated for finding gtkmm

---
+++ src/gtkmm/main.cpp
@@ -0,0 +1,27 @@
+/* File: main.cpp
+Copyright 2009 Spencer Jackson
+
+This file is part of Resolute
+is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Resolute is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Resolute.  If not, see <http://www.gnu.org/licenses/>.*/
+
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
+
+int main(int argc, char* argv[]) {
+  Gtk::Main kit(argc, argv);
+  Gtk::Window window;
+  Gtk::Main::run(window);
+
+  return 0;
+}
